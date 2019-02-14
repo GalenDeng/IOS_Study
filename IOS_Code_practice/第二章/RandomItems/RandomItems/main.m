1a0a672361e6a181c90cdc7dd0e642524edc348e
GalenDeng <827472476@qq.com>
2019-02-14 15:47:47 +0800

    ios练习

--- IOS_Code_practice/第二章/RandomItems/RandomItems/main.m
+++ IOS_Code_practice/第二章/RandomItems/RandomItems/main.m
@@ -7,11 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        [items addObject:@"one"];
+        [items addObject:@"two"];
+        [items addObject:@"three"];
+        
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // 快速枚举
+        for (NSString *item in items) {
+            NSLog(@"%@",item);
+        }
+        
+        // 传统for循环
+        /*for(int i =0;i< [items count];i++) {
+            NSString *item = [items objectAtIndex:i];
+            NSLog(@"%@",item);
+        }*/
+        BNRItem *item = [[BNRItem alloc] init];
+        //initlize
+        item.itemName =  @"Red Sofa";
+        [item setSerialNumber:@"A1B2C"];
+        [item setValueInDollars:100];
+        
+        NSLog(@"%@  %@ %@ %d",[item itemName],[item dateCreated],[item serialNumber],[item valueInDollars]);
+        
+        // 释放对象
+        items = nil;
     }
     return 0;
 }
